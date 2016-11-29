acc699801b5c4201b7174d0deb19ad5c9ebb1a09
Caesar <pankobe24@vip.qq.com>
2016-11-29 12:44:06 +0800

    Revert "Fix typo"

--- one/tamce/public/static/script/common.js
+++
@@ -1,5 +0,0 @@
-function showError(data)
-{
-	console.error(data);
-	alert(data);
-}
--- one/tamce/src/Views/Errors/400.php
+++
@@ -1,4 +0,0 @@
-<html>
-<head><title>400 Bad Request</title></head>
-<body><h1><center>Your request has something wrong and cannot be recognized.</center></h1></body>
-</html>
